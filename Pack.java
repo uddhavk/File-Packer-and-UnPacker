5fb11ad28bf032f353b3174cfa772006410ca104
Uddhav khatal <164346647+uddhavk@users.noreply.github.com>
2026-08-24 09:51:16 +0530

    Add files via upload

--- Pack.java
+++ Pack.java
@@ -1,91 +1,110 @@
-// Packing Application
-import java.io.*;
 import java.util.*;
-public static void main(String A[]) throws Exception
+import java.io.*;
+
+class Pack
 {
-    String Header = null;
-
-    byte key = 0x11;
-
-    int iRet = 0;
-    int i = 0, j = 0;
-
-    byte Buffer[] = new byte[1024];
-    byte bHeader[] = new byte[100];
-
-    Scanner sobj = new Scanner(System.in);
-
-    System.out.println("Enter the name of folder : ");
-    String FolderName = sobj.nextLine();
-
-    System.out.println("Enter the name of packed file : ");
-    String PackName = sobj.nextLine();
-
-    File SearchFolder = new File(FolderName);
-
-    if((SearchFolder.exists()) && (SearchFolder.isDirectory()))
+    public static void main(String A[]) throws Exception
     {
-        System.out.println("Folder is present");
 
-        File PackFile = new File(PackName);
-        PackFile.createNewFile();
+        // Variable declaration
+        String FolderName = null;
+        String PackName = null;
+        String Header = null;
 
-        // write the data in any type of file (text file,binary file,etc)
-        FileOutputStream SendData = new FileOutputStream(PackFile);
+        int i = 0, j = 0, iRet = 0;
+
+        Scanner sobj = null;
+
+        // Creating new File for storing data of other files
+        File PackFile = null;
+
+        // write the data in any type of file(text file, binary file, etc)
+        FileOutputStream SendData = null;
 
         // Read the data from file
         FileInputStream ReadData = null;
 
-        // Count the total number of files in searched folder
-        File fArr[] = SearchFolder.listFiles();
-        
-        System.out.println("Number of files in the folder are : "+fArr.length);
+        // Count total number of files in searched folder
+        File fArr[];
 
+        byte Buffer[] = new byte[1024];
+        byte bHeader[] = new byte[100];
+        byte key = 0x11;
 
-        for(i = 0; i < fArr.length; i++)
+        sobj = new Scanner(System.in);
+
+        System.out.println("Enter the name of folder : ");
+        FolderName = sobj.nextLine();
+
+        System.out.println("Enter the name for packed file : ");
+        PackName = sobj.nextLine();
+
+        File SearchFolder = new File(FolderName);
+
+        if((SearchFolder.exists()) && (SearchFolder.isDirectory()))
         {
-            ReadData = new FileInputStream(fArr[i]);
+            System.out.println("Folder is present");
 
-            // Checks Only .txt extension files
-            if(fArr[i].getName().endsWith(".txt"))
+            PackFile = new File(PackName);
+            PackFile.createNewFile();
+
+            SendData = new FileOutputStream(PackFile);
+            
+            fArr = SearchFolder.listFiles();
+
+            System.out.println("Number of files in the folder are : " + fArr.length);
+
+            for(i = 0; i < fArr.length; i++)
             {
-                // Header Function
-                Header = fArr[i].getName() + " " + fArr[i].length();
 
-                //It counts all characters, including spaces and special characters
-                for(j = Header.length(); j < 100; j++)
-                {
-                    Header = Header + " ";
-                }
-
-                // Converts the String into byte array
-                bHeader = Header.getBytes();
-
-                // Write Header into pack file
-                SendData.write(bHeader,0,100);
-
-                while((iRet = ReadData.read(Buffer)) != -1)
+                // Checks Only .txt extension files 
+                if(fArr[i].isFile() && fArr[i].getName().endsWith(".txt"))
                 {
 
-                    // Encryption
-                    for(j = 0; j < iRet; j++)
+                    ReadData = new FileInputStream(fArr[i]);
+
+                    // Header function
+                    Header = fArr[i].getName() + " " + fArr[i].length();
+
+                    // It counts all characters, Including spaces and special characters
+                    for(j = Header.length(); j < 100; j++)
                     {
-                        Buffer[j] = (byte)(Buffer[j] ^ key);
+                        Header = Header + " ";
                     }
 
-                    // write the files data into pack file
-                    SendData.write(Buffer,0,iRet);
-                }  // End of while
+                    // Converts the String into byte array
+                    bHeader = Header.getBytes();
 
-            } // End of if
-            ReadData.close();
-        } // End of for
-        SendData.close();
+                    // Write Header into pack file
+                    SendData.write(bHeader, 0, 100);
 
-    } // End of if
-    else
-    {
-        System.out.println("There is no such folder");
+                    // Reading Data 
+                    while((iRet = ReadData.read(Buffer)) != -1)
+                    {
+                        // Encryption
+                        for(j = 0; j < iRet; j++)               
+                        {
+                            Buffer[j] = (byte)(Buffer[j] ^ key);
+                        }
+
+                        // write the files data into pack file
+                        SendData.write(Buffer, 0, iRet);
+                    }   // End of while
+                    ReadData.close();
+
+                }   // End of if
+
+            } // End of for
+            System.out.println("Data Successfully Encrypted and Packed");
+
+
+            SendData.close();
+        }  // End of if
+        else
+        {
+            System.out.println("There is no such folder");
+        } // End of if
+
+        sobj.close();
     }
-    sobj.close();
 }
--- UnPack.java
+++ UnPack.java
@@ -1,83 +1,89 @@
-// UnPacking Application
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class UnPack
 {
-    public static void main() throws Exception
+    public static void main(String A[]) throws Exception
     {
-        int FileSize = 0;
-        int i = 0;
-        int iRet = 0;
-
-        byte Key = 0x11;
+        // Variable declaration
+        File PackFile = null;
+        File ExtractFile = null;
 
         Scanner sobj = null;
-        String FileName = null;
+        String PackFileName = null;
         String Header = null;
         String Tokens[] = null;
 
-        File FilePack = null;
-        File ExtractFile = null;
-
-        // write the data in any type of file (text file,binary file,etc)
-        FileInputStream ReadData = null;
-        // Read the data from file
+        // write the data in any type of file(text file, binary file, etc)
         FileOutputStream SendData = null;
 
+        // Read the data from file
+        FileInputStream ReadData = null;
+
         byte bHeader[] = new byte[100];
-        byte Buffer[] = null;
+        byte Buffer[];
+        byte key = 0x11;
+
+        int iRet = 0, i = 0;
+        int FileSize = 0;
 
         sobj = new Scanner(System.in);
 
-        System.out.println("Enter the name to packed file : ");
-        FileName = sobj.nextLine();
+        System.out.println("Enter the name of packed file : ");
+        PackFileName = sobj.nextLine();
 
-        FilePack = new File(FileName);
+        PackFile = new File(PackFileName);
 
-        if(FilePack.exists() == false)
+        if(PackFile.exists() == false)
         {
             System.out.println("Error : There is no such packed file");
             return;
         }
 
-        ReadData = new FileInputStream(FilePack);
+        ReadData = new FileInputStream(PackFile);
 
-        while((iRet = ReadData.read(bHeader,0,100)) != -1)
+        while((iRet = ReadData.read(bHeader, 0, 100)) != -1)
         {
             Header = new String(bHeader);
-
+            // Removing WhiteSpaces from header
             Header = Header.trim();
 
-            // Seprating the FileName and Its Size
+            // Seperating the Filename and its Size
             Tokens = Header.split(" ");
 
-            System.out.println("File Name : "+Tokens[0]);
-            System.out.println("File Size : "+Tokens[1]);
+            System.out.println("File Name : " + Tokens[0]);
+            System.out.println("File Size : " + Tokens[1]);
 
+            // Creating new file for writing encrypted data
             ExtractFile = new File(Tokens[0]);
-
             ExtractFile.createNewFile();
 
             SendData = new FileOutputStream(ExtractFile);
 
-            FileSize = Integer.parseInt(Tokens[1]); // Actual FileSize of its name
+            //Actual FileSize of its name
+            FileSize = Integer.parseInt(Tokens[1]);
 
             // Buffer for reading the data
             Buffer = new byte[FileSize];
 
             // Read from packed file
-            ReadData.read(Buffer,0,FileSize);
+            ReadData.read(Buffer, 0, FileSize);
 
             // Decrypt the data
-
             for(i = 0; i < FileSize; i++)
             {
-                Buffer[i] = (byte)(Buffer[i] ^ Key);
+                Buffer[i] = (byte)(Buffer[i] ^ key);
             }
-            
+
             // Write into extracted file
-            SendData.write(Buffer,0,FileSize);
-        }
+            SendData.write(Buffer, 0, FileSize);
+
+            SendData.close();
+        }   // End of while
+
+    ReadData.close();
+
+    sobj.close();
+        
     }
 }
